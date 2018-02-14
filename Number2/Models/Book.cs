938d698090f3a0ca067aa09b4b3e6d1bac354231
levxset <levxset@gmail.com>
2018-02-14 13:51:22 +0200

    1

---
+++ Number2/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Number2.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+        [Required]
+        public int BookId { get; set; }
+
+        [Display(Name = "Book Name")]
+        [MaxLength(100, ErrorMessage = "Book Name must be 100 characters or less"), MinLength(5)]
+        public string BookName { get; set; }
+
+        public int Pages { get; set; }
+
+        public string Content { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public virtual ICollection<Author> Authors { get; set; }
+
+
+    }
+}
---
+++ Number2/Models/Genre.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Number2.Models
+{
+    public enum Genre
+    {
+        [Display(Name = "Comedy")]
+        Comedy,
+        [Display(Name = "Drama")]
+        Drama,
+        [Display(Name = "Horror")]
+        Horror,
+        [Display(Name = "Realism")]
+        Realism,
+        [Display(Name = "Romance")]
+        Romance,
+        [Display(Name = "Satire")]
+        Satire,
+        [Display(Name = "Tragedy")]
+        Tragedy,
+        [Display(Name = "Tragicomedy")]
+        Tragicomedy,
+        [Display(Name = "Fantasy")]
+        Fantasy,
+        [Display(Name = "Mythology")]
+        Mythology,
+        [Display(Name = "Adventure")]
+        Adventure,
+    }
+}
