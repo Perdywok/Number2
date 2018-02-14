aa882deb38793167c79d52a21e4ad5b3b4921141
levxset <levxset@gmail.com>
2018-02-14 13:37:33 +0200

    Add project files.

---
+++ Number2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Number2.MvcApplication" Language="C#" %>
---
+++ Number2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Number2.Startup))]
+namespace Number2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
